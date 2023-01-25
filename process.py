8b0d5fd664c2e984148c55180fd011ab7dab6ae0
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-01-25 11:42:43 -0500

    fix format

--- process.py
+++ process.py
@@ -2,18 +2,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-with open("default.results", "r") as f:
-    content = f.read()
-    values = np.array([int(x) for x in content.split(',')[:-1]])
-    print(values)
+def main():
+    with open("default.results", "r") as f:
+        content = f.read()
+        values = np.array([int(x) for x in content.split(',')[:-1]])
+        print(values)
 
-print(values)
-fig, ax = plt.subplots()
-#sns.kdeplot(values, ax=ax)
-plt.xlim(right=20000)
-sns.histplot(values, binwidth=1000, stat='proportion', ax=ax)
-sns.ecdfplot(values, color='orange', stat='proportion', ax=ax)
-#plt.hist(values, density=True, bins=30)
-plt.ylabel('Density')
-plt.xlabel('Transmission time (us)')
-plt.savefig('hist.png')
+    print(values)
+    fig, ax = plt.subplots()
+    plt.xlim(right=20000)
+    sns.histplot(values, binwidth=1000, stat='proportion', ax=ax)
+    sns.ecdfplot(values, color='orange', stat='proportion', ax=ax)
+    plt.ylabel('Density')
+    plt.xlabel('Transmission time (us)')
+    plt.savefig('hist.png')
+
+
+if __name__ == '__main__':
+    main()
