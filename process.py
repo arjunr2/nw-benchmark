18b5ff6c63c97a6668c04256940ec0dc72f31f31
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-02-09 09:43:39 -0500

    Added collection bin to plot

--- process.py
+++ process.py
@@ -3,18 +3,39 @@ import seaborn as sns
 import numpy as np
 from pathlib import Path
 
+limit = 20000
+tickwidth = 2000
+binwidth = 1000
+
 def plot(infile, outfile):
     with open(infile, "r") as f:
         content = f.read()
         values = np.array([int(x) for x in content.split(',')[:-1]])
 
     print(values)
+
     fig, ax = plt.subplots()
-    plt.xlim(right=10000)
-    sns.histplot(values, binwidth=1000, stat='proportion', ax=ax)
-    sns.ecdfplot(values, color='orange', stat='proportion', linewidth=3, ax=ax)
+
+    # Obtain bins and clipped values > limit
+    bins = np.arange(0, limit + binwidth, binwidth) 
+    clipped_values = np.clip(values, bins[0], bins[-1])
+
+    # Generate plot
+    sns.histplot(clipped_values, bins=bins, stat='proportion', ax=ax)
+    sns.ecdfplot(clipped_values, color='orange', stat='proportion', linewidth=3, ax=ax)
+
+    # Set plot labels and width
+    plt.xlim(right=limit+2*binwidth)
+
+    ticks = np.arange(0, limit + tickwidth, tickwidth)
+    xlabels = (ticks//1000).astype(str);
+    xlabels[-1] = ">" + xlabels[-1]
+    
+    plt.xticks(ticks)
+    ax.set_xticklabels(xlabels)
     plt.ylabel('Density')
-    plt.xlabel('Transmission time (us)')
+    plt.xlabel('Transmission time (ms)')
+
     plt.savefig(outfile)
     plt.close()
 
